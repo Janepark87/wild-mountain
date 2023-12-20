766615c7e9bab38caaefae8a2a96692cfef9766b
Jpark <web.jpark@gmail.com>
2023-12-19 19:05:19 -0700

    set up and initializ the supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { Heading } from '../components/Heading';
 import { Row } from '../components/Row';
+import { getCabins } from '../services/apiCabins';
 
 export default function Cabins() {
+	useEffect(() => {
+		getCabins().then((data) => console.log(data));
+	}, []);
+
 	return (
 		<Row type="horizontal">
 			<Heading as="h1">Cabins</Heading>
