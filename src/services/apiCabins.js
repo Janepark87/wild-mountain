64f89cc96c8d9762a6425f945c221945d61c3895
Jpark <web.jpark@gmail.com>
2023-12-22 12:55:47 -0700

    uploading images to supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,12 +10,32 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-	const { data, error } = await supabase.from('cabins').insert([newCabin]).select();
+	// create a new image path
+	const randomInteger = Math.floor(Date.now() + 1);
+	const imageName = `${randomInteger}-${newCabin.image.name}`.replaceAll('/', '');
+	const imagePath = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/cabin-images/${imageName}`;
+
+	// create a new cabin
+	const { data, error } = await supabase
+		.from('cabins')
+		.insert([{ ...newCabin, image: imagePath }])
+		.select();
 
 	if (error) {
 		console.error(error);
 		throw new Error('Cabins could not be created.');
 	}
+
+	// upload image into supabase
+	const { error: storageError } = await supabase.storage.from('cabin-images').upload(imageName, newCabin.image);
+
+	// delete the cabin If there was an error uploading image
+	if (storageError) {
+		await supabase.from('cabins').delete().eq('id', data.id);
+		console.error(storageError);
+		throw new Error('Cabin image could not be upload and the cabin was not created.');
+	}
+
 	return data;
 }
 
