7694bdd893658024d4a0ece284baa39b7de2a0fb
Jpark <web.jpark@gmail.com>
2023-12-28 14:18:45 -0700

    updating settings data

--- src/features/settings/UpdateSettingsForm2.jsx
+++ src/features/settings/UpdateSettingsForm2.jsx
@@ -7,6 +7,7 @@ import Spinner from '../../components/Spinner';
 export default function UpdateSettingsForm() {
 	const { settings = {}, isSettingLoading } = useSettingQuery();
 	const { minBookingLength, maxBookingLength, maxGuestsPerBooking, breakfastPrice } = settings;
+
 	const { updateSettingMutate } = useUpdateSetting();
 
 	const handleUpdate = (e) => {
@@ -31,7 +32,7 @@ export default function UpdateSettingsForm() {
 	return (
 		<Form>
 			<FormRow label="Minimum nights/booking">
-				<Input type="number" id="min-nights" name="minBookingLength" defaultValue={minBookingLength} onBlur={(e) => handleUpdate(e)} />
+				<Input type="number" id="min-nights" defaultValue={minBookingLength} onBlur={(e) => handleUpdate(e)} />
 			</FormRow>
 			<FormRow label="Maximum nights/booking">
 				<Input type="number" id="max-nights" name="maxBookingLength" defaultValue={maxBookingLength} onBlur={(e) => handleUpdate(e)} />
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,6 @@
 import Heading from '../components/Heading';
 import Row from '../components/Row';
-import UpdateSettingsForm from '../features/settings/UpdateSettingsForm2';
+import UpdateSettingsForm from '../features/settings/UpdateSettingsForm2-trial';
 
 export default function Settings() {
 	return (
