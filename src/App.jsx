e56b05f8177dc10f15f6d40273ea25f0acf774e1
Jpark <web.jpark@gmail.com>
2024-06-12 07:15:30 -0600

    Organized the import/export files - pages, layouts folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import AppLayout from './layouts/AppLayout';
-import Dashboard from './pages/Dashboard';
-import Bookings from './pages/Bookings';
-import Booking from './pages/Booking';
-import Cabins from './pages/Cabins';
-import Users from './pages/Users';
-import Settings from './pages/Settings';
-import Account from './pages/Account';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
-import Checkin from './pages/Checkin';
-import ProtectedRoute from './layouts/ProtectedRoute';
+import { AppLayout, ProtectedRoute } from './layouts';
+import { Account, Booking, Bookings, Cabins, Checkin, Dashboard, Login, PageNotFound, Settings, Users } from './pages';
 
 export default function App() {
 	return (
@@ -22,18 +12,18 @@ export default function App() {
 							<AppLayout />
 						</ProtectedRoute>
 					}>
-					<Route index element={<Navigate to="dashboard" replace />} />
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="bookings" element={<Bookings />} />
-					<Route path="bookings/:bookingId" element={<Booking />} />
-					<Route path="checkin/:bookingId" element={<Checkin />} />
-					<Route path="cabins" element={<Cabins />} />
-					<Route path="users" element={<Users />} />
-					<Route path="settings" element={<Settings />} />
-					<Route path="account" element={<Account />} />
+					<Route index element={<Navigate to='dashboard' replace />} />
+					<Route path='dashboard' element={<Dashboard />} />
+					<Route path='bookings' element={<Bookings />} />
+					<Route path='bookings/:bookingId' element={<Booking />} />
+					<Route path='checkin/:bookingId' element={<Checkin />} />
+					<Route path='cabins' element={<Cabins />} />
+					<Route path='users' element={<Users />} />
+					<Route path='settings' element={<Settings />} />
+					<Route path='account' element={<Account />} />
 				</Route>
-				<Route path="login" element={<Login />} />
-				<Route path="*" element={<PageNotFound />} />
+				<Route path='login' element={<Login />} />
+				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/components/index.js
@@ -0,0 +1,40 @@
+// base UI component
+export { default as Logo } from './Logo';
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as ButtonGroup } from './ButtonGroup';
+export { default as Pagination } from './Pagination';
+export { default as ErrorFallback } from './ErrorFallback';
+export { default as Spinner } from './Spinner';
+export { default as Empty } from './Empty';
+export { default as Flag } from './Flag';
+export { default as DarkModeToggle } from './DarkModeToggle';
+
+// block components
+export { default as Modal } from './Modal';
+export { default as EllipsisDropdown } from './EllipsisDropdown';
+export { default as DataItem } from './DataItem';
+export { default as ConfirmDelete } from './ConfirmDelete';
+
+// layout & navigation
+export { default as Container } from './Container';
+export { default as Heading } from './Heading';
+export { default as HeaderMenu } from './HeaderMenu';
+
+// inputs
+export { default as Input } from './Input';
+export { default as Textarea } from './Textarea';
+export { default as Checkbox } from './Checkbox';
+export { default as Select } from './Select';
+export { default as FileInput } from './FileInput';
+
+// sorts & filters
+export { default as SortBy } from './SortBy';
+export { default as Filter } from './Filter';
+
+// Form & table
+export { default as Form } from './Form';
+export { default as FormRow } from './FormRow';
+export { default as Row } from './Row';
+export { default as Table } from './Table';
+export { default as TableOperations } from './TableOperations';
